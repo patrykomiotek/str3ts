e907d220e42973177e5087e37789609bbbcf597c
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-11-22 10:02:05 +0100

    feat: enums

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -1,3 +1,10 @@
+// type Role = 'admin' | 'user' | 'super-admin';
+enum Role {
+  ADMIN = "admin",
+  USER = "user",
+  SUPER_ADMIN = "super-admin",
+}
+
 interface User {
   id: number;
   firstName: string;
@@ -8,13 +15,12 @@ interface User {
    * - 'user'
    * - 'super-admin'
    */
-  role: string;
+  role: Role;
 }
 
 export const defaultUser: User = {
   id: 1,
   firstName: "John",
   lastName: "Doe",
-  // @ts-expect-error
-  role: "I_SHOULD_NOT_BE_ALLOWED",
+  role: Role.USER,
 };
