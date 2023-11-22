33c1c16bb932a0284be5a1a322aec84f01dd94cc
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-11-22 10:29:47 +0100

    feat: try catch

--- src/11-catch-problem.ts
+++ src/11-catch-problem.ts
@@ -1,12 +1,19 @@
 import { expect, it } from "vitest";
 
+class HttpError extends Error {}
+class ValidationError extends Error {}
+
 const tryCatchDemo = (state: "fail" | "succeed") => {
   try {
     if (state === "fail") {
       throw new Error("Failure!");
+      // throw new Mieso("Failure!");
     }
   } catch (e) {
-    return e.message;
+    if (e instanceof HttpError) {
+    } else if (e instanceof Error) {
+      return e.message;
+    }
   }
 };
 
